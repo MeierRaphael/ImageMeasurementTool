1eda6851afdb8df2cf0e8bdb651d23d2b45cd124
Raphi <raphael.meier1@students.fhnw.ch>
2024-06-06 19:33:56 +0200

    Init

--- src/main/java/ch/fhnw/prp/amrs/logic/CallbackFunction.java
+++
@@ -1,5 +0,0 @@
-package ch.fhnw.prp.amrs.logic;
-
-public interface CallbackFunction {
-    void apply();
-}
--- src/main/java/ch/fhnw/prp/amrs/presentation/StateObserver.java
+++
@@ -1,5 +0,0 @@
-package ch.fhnw.prp.amrs.presentation;
-
-public interface StateObserver {
-    void stateChanged();
-}
